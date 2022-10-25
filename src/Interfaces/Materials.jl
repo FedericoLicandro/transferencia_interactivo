a985eb4f997d56e1f272e216c904a57270433c56
Federico Licandro <licandro.federico@gmail.com>
2022-10-25 19:59:16 -0300

    Materials update

--- src/Interfaces/Materials.jl
+++ src/Interfaces/Materials.jl
@@ -1,1 +1,18 @@
 # Materiales, como definirlos, que propiedades tienen y que funciones se les aplican
+
+
+abstract type AbstractMaterial end
+abstract type AbstractSolid  <:AbstractMaterial end
+abstract type AbstractFluid <:AbstractMaterial end
+
+
+struct Metal <:AbstractSolid
+    ρ::Real
+    C::Real
+    k::Real
+    α::Real
+    function Metal(ρ,C,k)
+        new(ρ,C,k,k/(ρ*C*1000))
+    end
+end
+    #prueba
